fe400393d7e45d2f3c1e65943e4b0a3622701eff
SergioFoma <s.kuzminsky.2007@gmail.com>
2025-08-15 21:18:41 +0400

    Добавил для if/if else фигурные скобки + функция getNumber;
    Замены:
    
    в функции getint: res -> result
    i -> sizeArray
    j -> indexArray
    k -> sizeNumber
    
    Везде, где стояло if/ if else добавил фигурные скобки
    
    Добавил функцию getNumbrer

--- learning.cpp
+++ learning.cpp
@@ -9,8 +9,12 @@ char myStack[ MAXSIZE ]; // Стек
 int Size = 0; // Размера стека
 
 void push(char el);
+
 char pop();
-int getint( char number[], int k);
+
+int getint( char number[], int sizeNumber);
+
+int getNumber( char number[], int sizeNumber, int parametr, int sign);
 
 int main( void ) {
 
@@ -19,49 +23,51 @@ int main( void ) {
 
     char Array[ MAXSIZE ];
 
-    int C, i = 0, j; // индексы i и j для массива и переменная c для считывания
+    int element = 0, sizeArray = 0, indexArray = 0; // индексы i и j для массива и переменная c для считывания
 
-    while ( ( C = getchar() ) != '\n' && i < MAXSIZE) Array[i++] = C;
+    while ( ( element = getchar() ) != '\n' && sizeArray < MAXSIZE) {
 
-    int a = 1,b = 1,c  = 1; // коэффициенты квадратного уравнения ( по умолчанию 1)
+        Array[ sizeArray++ ] = element;
+    }
+
+    int a = 1, b = 1, c  = 1; // коэффициенты квадратного уравнения ( по умолчанию 1)
 
     int sign = 1; // Знак коэффициентов
 
-    for ( j = 0; j < i; j++) {
+    for (; indexArray < sizeArray; indexArray++) {
 
         char number[ MAXSIZE ];
-        int k = 0;
+        int sizeNumber = 0;
 
-        if ( isdigit( Array[j] ) && Array[ j - 1] != '^' ) { // Добавляем все цифры кроме степени икса
-            push( Array[j] );
+        if ( isdigit( Array[ indexArray ] ) && Array[ indexArray - 1] != '^' ) { // Добавляем все цифры кроме степени икса
+
+            push( Array[ indexArray ] );
         }
 
 
-        if ( Array[j-1] != 'x' && (Array[j] == '+' || Array[j] == '-' || ( j == i - 1 && isdigit(Array[j]) ) ) ) { // Подсчет коэффициента c
+        if ( Array[ indexArray-1 ] != 'x' && (Array[ indexArray ] == '+' || Array[ indexArray ] == '-' || ( indexArray == sizeArray - 1 && isdigit(Array[ indexArray ]) ) ) ) { // Подсчет коэффициента c
 
-            while( Size > 0 ) number[ k++ ] = pop();
-            if( k != 0 ) c = getint( number, k ) * sign;
-            else c *= sign;
+            c = getNumber( number, sizeNumber, c, sign);
 
         }
 
-        else if ( Array[j] == 'x' && Array[ j + 1 ] == '^') { // Подсчет коэффициента a
+        else if ( Array[ indexArray ] == 'x' && Array[ indexArray + 1 ] == '^') { // Подсчет коэффициента a
 
-            while( Size > 0 ) number[ k++ ] = pop();
-            if( k != 0 ) a = getint( number, k ) * sign;
-            else a *= sign;
+            a = getNumber( number, sizeNumber, a, sign);
         }
 
-        else if( Array[j] == 'x') { // Подсчет коэффициента b
+        else if( Array[ indexArray ] == 'x') { // Подсчет коэффициента b
 
-            while( Size > 0 ) number[ k++ ] = pop();
-            if( k != 0 ) b = getint( number, k ) * sign;
-            else b *= sign;
+            b = getNumber( number, sizeNumber, b, sign );
         }
 
-        if ( Array[j] == '-') sign = -1;
-                                              // меняем знак, если его нашли
-        else if ( Array[j] == '+') sign = 1;
+        if ( Array[ indexArray ] == '-') {
+            sign = -1;
+        }                                       // меняем знак, если его нашли
+
+        else if ( Array[ indexArray ] == '+') {
+            sign = 1;
+        }
     }
 
     int D = b * b - 4 * a * c;
@@ -72,7 +78,10 @@ int main( void ) {
 
     }
 
-    else if ( a == 0) printf("Уравнение не квадратное. Старший коэффициент равен нулю");
+    else if ( a == 0) {
+
+        printf("Уравнение не квадратное. Старший коэффициент равен нулю");
+    }
 
     else {
 
@@ -82,9 +91,13 @@ int main( void ) {
 
         printf("a: %d b: %d c: %d\n", a, b, c);
 
-        if ( x1 == x2) printf("Корни совпадают и равны %.1f", x1);
+        if ( x1 == x2) {
+            printf("Корни совпадают и равны %.1f", x1);
+        }
 
-        else printf("Меньший корень уравнения: %.1f\nБольший корень уравнения: %.1f", x1, x2);
+        else {
+            printf("Меньший корень уравнения: %.1f\nБольший корень уравнения: %.1f", x1, x2);
+        }
     }
     return 0;
 }
@@ -95,16 +108,24 @@ void push( char el) {
 char pop() {
     return myStack[ --Size ];
 }
-int getint( char number[], int k ) {
+int getint( char number[], int sizeNumber ) {
 
-    int i;
-    for ( i = 0; i < ( k / 2); i++) { // цикл для reverse
-        int tmp = number[i];
-        number[i] = number[ k - 1 - i];
-        number[ k - 1 - i] = tmp;
+    int indexNumber = 0;
+    for ( ; indexNumber < ( sizeNumber / 2); indexNumber++) { // цикл для reverse
+        int tmp = number[ indexNumber ];
+        number[ indexNumber ] = number[ sizeNumber - 1 - indexNumber];
+        number[ sizeNumber - 1 - indexNumber] = tmp;
     }
 
-    int res = 0;
-    for ( i = 0; i < k; i++) res = 10 * res + ( number[i] - '0'); // перевод строки в числовый формат
-    return res;
+    int result = 0;
+    for ( indexNumber = 0; indexNumber < sizeNumber; indexNumber++) result = 10 * result + ( number[ indexNumber ] - '0'); // перевод строки в числовый формат
+    return result;
+
+}
+int getNumber( char number[], int sizeNumber, int parametr, int sign) {
+
+    while( Size > 0 ) number[ sizeNumber++ ] = pop();
+    if( sizeNumber != 0 ) return ( getint( number, sizeNumber ) * sign );
+    else return ( parametr *= sign );
+
 }
