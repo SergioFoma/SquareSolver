a4d85b03fb6de55e268818f9a9c6589b7f58a426
SergioFoma <s.kuzminsky.2007@gmail.com>
2025-08-15 16:41:47 +0400

    Добавил проверку на совпадение корней и коэффициенты a, b, c.

--- learning.cpp
+++ learning.cpp
@@ -5,8 +5,8 @@
 
 #define MAXSIZE 100
 
-char myStack[ MAXSIZE ];
-int Size = 0;
+char myStack[ MAXSIZE ]; // Стек
+int Size = 0; // Размера стека
 
 void push(char el);
 char pop();
@@ -16,25 +16,28 @@ int main( void ) {
 
     printf("Правило ввода: Если хотите возвести в степень, введите символ '^'\n\nЕсли хотите умножить число на неизвестную  переменную, введите число с учетом знака, а затем переменнную без пробелов\n\n");
     printf("Пример: 5 умножить на x эквивалетно записи 5x, x возвести в степень 2 эквивалентно записи 2^x\n\n");
+
     char Array[ MAXSIZE ];
-    int C, i = 0, j; // индекс i для массива и переменная c для считывания
+
+    int C, i = 0, j; // индексы i и j для массива и переменная c для считывания
 
     while ( ( C = getchar() ) != '\n' && i < MAXSIZE) Array[i++] = C;
 
-    int a = 1,b = 1,c  = 1; // коэффициенты квадратного уравнения
-    int sign = 1;
+    int a = 1,b = 1,c  = 1; // коэффициенты квадратного уравнения ( по умолчанию 1)
+
+    int sign = 1; // Знак коэффициентов
 
     for ( j = 0; j < i; j++) {
 
         char number[ MAXSIZE ];
         int k = 0;
 
-        if ( isdigit( Array[j] ) && Array[ j - 1] != '^' ) {
+        if ( isdigit( Array[j] ) && Array[ j - 1] != '^' ) { // Добавляем все цифры кроме степени икса
             push( Array[j] );
         }
 
 
-        if ( Array[j-1] != 'x' && (Array[j] == '+' || Array[j] == '-' || ( j == i - 1 && isdigit(Array[j]) ) ) ) {
+        if ( Array[j-1] != 'x' && (Array[j] == '+' || Array[j] == '-' || ( j == i - 1 && isdigit(Array[j]) ) ) ) { // Подсчет коэффициента c
 
             while( Size > 0 ) number[ k++ ] = pop();
             if( k != 0 ) c = getint( number, k ) * sign;
@@ -42,14 +45,14 @@ int main( void ) {
 
         }
 
-        else if ( Array[j] == 'x' && Array[ j + 1 ] == '^') {
+        else if ( Array[j] == 'x' && Array[ j + 1 ] == '^') { // Подсчет коэффициента a
 
             while( Size > 0 ) number[ k++ ] = pop();
             if( k != 0 ) a = getint( number, k ) * sign;
             else a *= sign;
         }
 
-        else if( Array[j] == 'x') {
+        else if( Array[j] == 'x') { // Подсчет коэффициента b
 
             while( Size > 0 ) number[ k++ ] = pop();
             if( k != 0 ) b = getint( number, k ) * sign;
@@ -57,25 +60,31 @@ int main( void ) {
         }
 
         if ( Array[j] == '-') sign = -1;
-
+                                              // меняем знак, если его нашли
         else if ( Array[j] == '+') sign = 1;
     }
 
     int D = b * b - 4 * a * c;
+
     if ( D < 0) {
+
         printf("Действительных корней нет");
-        //printf("%d %d %d\n", a, b, c);
+
     }
+
     else if ( a == 0) printf("Уравнение не квадратное. Старший коэффициент равен нулю");
+
     else {
 
         double x1 = ( - b - sqrt( D ) ) / (2 * a);
 
         double x2 = ( - b + sqrt( D ) ) / (2 * a);
 
-        //printf("%d %d %d\n", a, b, c);
+        printf("a: %d b: %d c: %d\n", a, b, c);
 
-        printf("Меньший корень уравнения: %.1f\nБольший корень уравнения: %.1f", x1, x2);
+        if ( x1 == x2) printf("Корни совпадают и равны %.1f", x1);
+
+        else printf("Меньший корень уравнения: %.1f\nБольший корень уравнения: %.1f", x1, x2);
     }
     return 0;
 }
@@ -89,14 +98,14 @@ char pop() {
 int getint( char number[], int k ) {
 
     int i;
-    for ( i = 0; i < ( k / 2); i++) {
+    for ( i = 0; i < ( k / 2); i++) { // цикл для reverse
         int tmp = number[i];
         number[i] = number[ k - 1 - i];
         number[ k - 1 - i] = tmp;
     }
 
     int res = 0;
-    for ( i = 0; i < k; i++) res = 10 * res + ( number[i] - '0');
+    for ( i = 0; i < k; i++) res = 10 * res + ( number[i] - '0'); // перевод строки в числовый формат
     return res;
 
 }
