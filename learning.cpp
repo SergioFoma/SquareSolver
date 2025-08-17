78fbd78b675e7ab389d7ea08538ad3d96e223ad9
SergioFoma <s.kuzminsky.2007@gmail.com>
2025-08-17 13:47:12 +0400

    Чуть поправил оформление кода, внедрил проверку на корректность в цикл в main

--- learning.cpp
+++ learning.cpp
@@ -7,13 +7,13 @@ void push(char el, char* myStack, int* stackSize);
 
 char pop( char* myStack, int* stackSize);
 
-double getint( char number[], int sizeNumber);
+double getInt( char number[], int sizeNumber);
 
 double getNumber( char number[], int sizeNumber, double parametr, int numberSign, char* myStack, int* stackSize);
 
-int correctInput( char* inputString, int* inputSize, const int* maxArraySize);
+int correctInput( char* inputString, int inputIndex );
 
-int main( void ) {
+int main( ) {
 
     printf("Правило ввода: Если хотите возвести в степень, введите символ '^'\n\n"
     "Если хотите умножить число на неизвестную  переменную, введите число с учетом знака, а затем переменнную без пробелов\n\n"
@@ -24,11 +24,15 @@ int main( void ) {
     char myStack[ maxArraySize ]; // Стек
     int stackSize = 0; // Размера стека
 
-    char inputString[ maxArraySize ];
+    char inputString[ maxArraySize ]{};
 
-    int inputSize = 0; // размер строки ввода
+    scanf("%s", inputString);
 
-    if ( !correctInput( inputString, &inputSize, &maxArraySize) ) { // Считывание и проверка на корректноть
+    int inputSize = strlen( inputString) ; // размер строки ввода
+
+    if( inputSize == 0) {
+
+        printf("\nВведена пустая строка.");
         return 0;
     }
 
@@ -38,14 +42,19 @@ int main( void ) {
 
     for (int inputStringIndex = 0; inputStringIndex < inputSize; inputStringIndex++) {
 
-        char number[ maxArraySize ];
+        if( !correctInput( inputString, inputStringIndex ) ) { // Проверка на корректность ввода
+
+            return 0;
+        }
+
+        char number[ maxArraySize ]{};
         int sizeNumber = 0;
+
 
         if ( (isdigit( inputString[ inputStringIndex ] ) || inputString[ inputStringIndex] == '.') && inputString[ inputStringIndex - 1] != '^' ) { // Добавляем все цифры кроме степени икса
 
             push( inputString[ inputStringIndex ], myStack, &stackSize );
         }
-
 
         if ( inputString[ inputStringIndex-1 ] != 'x' && (inputString[ inputStringIndex ] == '+'
         || inputString[ inputStringIndex ] == '-'
@@ -105,7 +114,7 @@ int main( void ) {
             printf("Меньший корень уравнения: %.1f\nБольший корень уравнения: %.1f", x1, x2);
         }
     }
-    return 0;
+
 }
 
 void push( char el, char* myStack, int* stackSize) {
@@ -116,7 +125,7 @@ char pop( char* myStack, int* stackSize) {
     (*stackSize)--;
     return *( myStack + (*stackSize) );
 }
-double getint( char number[], int sizeNumber ) {
+double getInt( char number[], int sizeNumber ) {
 
     int indexNumber = 0;
     for ( ; indexNumber < ( sizeNumber / 2); indexNumber++) { // цикл для reverse
@@ -145,7 +154,7 @@ double getint( char number[], int sizeNumber ) {
     }
 
 
-    return ( result / power );
+    return result / power;
 
 }
 double getNumber( char number[], int sizeNumber, double parametr, int numberSign, char* myStack, int* stackSize) {
@@ -155,77 +164,61 @@ double getNumber( char number[], int sizeNumber, double parametr, int numberSign
         number[ sizeNumber++ ] = pop( myStack, stackSize );
     }
 
-    if( sizeNumber != 0 ) return ( getint( number, sizeNumber ) * numberSign );
+    if( sizeNumber != 0 ) return ( getInt( number, sizeNumber ) * numberSign );
     else return ( parametr *= numberSign );
 }
-int correctInput( char* inputString, int*inputSize, const int* maxArraySize) {
+int correctInput( char* inputString, int inputIndex ) {
 
-    int inputRead = 0;
+    int countX = 0, countXX = 0;
 
-    while ( ( inputRead = getchar() ) != '\n' && *inputSize < *maxArraySize) { // Считывание строки
+    if ( *(inputString + inputIndex) == 'x') {
 
-        inputString[ (*inputSize)++ ] = inputRead;
-
-        if ( *inputSize == *maxArraySize) {
-            printf("Введенная строка имеет максимально возможною длинну. Больше символов ввести нельзя\n\n");
-        }
+        ++countX;
     }
 
-    // Проверка на корректность ввода
+    else if( *(inputString + inputIndex) == '^') {
 
-    if ( *inputSize == 0 ) {
+        ++countXX;
+    }
 
-        printf("\nОшибка. Введена пустая строка.\n");
+    if ( !( isdigit( *(inputString + inputIndex) ) ) && ( *(inputString + inputIndex) != 'x' )
+    && ( *(inputString + inputIndex) != '+') && (*(inputString + inputIndex) != '-')
+    && (*(inputString + inputIndex) != '^') && ( *(inputString + inputIndex) != '.' ) ) {
+
+        printf("\nОшибка. Введен неверный символ: %c\n", *(inputString + inputIndex) );
         return 0;
     }
 
-    int countX = 0, countXX = 0;
-    for ( int inputIndex = 0 ; inputIndex < *inputSize; inputIndex++) {
+    else if ( *( inputString + inputIndex) == 'x' && isdigit( *(inputString + inputIndex + 1) ) ) {
 
-        if ( *(inputString + inputIndex) == 'x') {
-
-            ++countX;
-        }
-
-        else if( *(inputString + inputIndex) == '^') {
-
-            ++countXX;
-        }
-
-        if ( !( isdigit( *(inputString + inputIndex) ) ) && ( *(inputString + inputIndex) != 'x' )
-        && ( *(inputString + inputIndex) != '+') && (*(inputString + inputIndex) != '-')
-        && (*(inputString + inputIndex) != '^') && ( *(inputString + inputIndex) != '.' ) ) {
-
-            printf("\nОшибка. Введен неверный символ: %c\n", *(inputString + inputIndex) );
-            return 0;
-        }
-
-        else if ( *( inputString + inputIndex) == 'x' && isdigit( *(inputString + inputIndex + 1) ) ) {
-
-            printf("\nОшибка. Введен неверный формат квадратного уравнения. Коэффициенты должны быть до x.\n");
-            return 0;
-        }
-
-        else if( ( *( inputString + inputIndex ) == '+' || *( inputString + inputIndex ) == '-' || *(inputString + inputIndex) == '.')
-        && ( *( inputString + inputIndex + 1 ) == '+' || *( inputString + inputIndex + 1) == '-' || *(inputString + inputIndex + 1) == '.' ) ) {
-
-            printf("\nОшибка. Введен лишний символ %c\n", *( inputString + inputIndex + 1) );
-            return 0;
-        }
-
-        else if ( countX > 2 || countXX > 1 ) {
-
-            printf("\nОшибка. Введен неверный формат квадратного уравнения.\n");
-            return 0;
-        }
-
-        else if( *(inputString + inputIndex) == '^' && *(inputString + inputIndex + 1) != '2') {
-
-            printf("\nОшибка. Уравнение не второй степени.");
-            return 0;
-        }
-
-
+        printf("\nОшибка. Введен неверный формат квадратного уравнения. Коэффициенты должны быть до x.\n");
+        return 0;
     }
+
+    else if( ( *( inputString + inputIndex ) == '+' || *( inputString + inputIndex ) == '-' || *(inputString + inputIndex) == '.')
+    && ( *( inputString + inputIndex + 1 ) == '+' || *( inputString + inputIndex + 1) == '-' || *(inputString + inputIndex + 1) == '.' ) ) {
+
+        printf("\nОшибка. Введен лишний символ %c\n", *( inputString + inputIndex + 1) );
+        return 0;
+    }
+
+    else if ( countX > 2 || countXX > 1 ) {
+
+        printf("\nОшибка. Введен неверный формат квадратного уравнения.\n");
+        return 0;
+    }
+
+    else if( *(inputString + inputIndex) == '^' && *(inputString + inputIndex + 1) != '2') {
+
+        printf("\nОшибка. Уравнение не второй степени.");
+        return 0;
+    }
+
+    else if( *(inputString + inputIndex) == '^'&& *(inputString + inputIndex - 1) != 'x') {
+
+        printf("\nОшибка. Введен неверный формат квадратного уравнения.\n");
+        return 0;
+    }
+
     return 1;
 }
