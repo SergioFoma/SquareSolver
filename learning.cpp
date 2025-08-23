2039d0db4b6dad968432d6d211caf08fcf3d0f18
SergioFoma <s.kuzminsky.2007@gmail.com>
2025-08-23 16:19:53 +0300

    Unit тесты добавил

--- learning.cpp
+++ learning.cpp
@@ -14,7 +14,7 @@ enum RootsCount {
 
 }; // возможное количество решений
 
-const double delta = 1e-8; // Дельта окрестность
+const double delta = 1e-5; // Дельта окрестность
 
 struct Coefficients {
 
@@ -23,7 +23,7 @@ struct Coefficients {
     double c;
 };
 
-struct AnswerQuestion {
+struct SolveResult {
 
     double x1;
     double x2;
@@ -31,13 +31,20 @@ struct AnswerQuestion {
     RootsCount countRoots;
 };
 
-AnswerQuestion solveEquation( struct Coefficients coefficients );
+SolveResult solveEquation( Coefficients coefficients );
 
-void solveSquare( struct Coefficients coefficients, double* x1, double* x2, RootsCount* countRoots );
+void solveSquare( Coefficients coefficients, double* x1, double* x2, RootsCount* countRoots );
 
-void solveLinear( struct Coefficients coefficients, double* x1, double* x2, RootsCount* countRoots );
+void solveLinear( Coefficients coefficients, double* x1, double* x2, RootsCount* countRoots );
 
-void printResult( struct Coefficients coefficients, struct AnswerQuestion answer );
+void printResult( Coefficients coefficients, SolveResult answer );
+
+void test();
+
+int testSolve( SolveResult truResult, SolveResult* actualResult );
+
+void printFail( Coefficients coeff, SolveResult truResult, SolveResult* actualResult );
+
 
 int main( ) {
 
@@ -52,7 +59,9 @@ int main( ) {
         return 0;
     }
 
-    AnswerQuestion answer = solveEquation( coefficients );
+    test();
+
+    SolveResult answer = solveEquation( coefficients );
     printResult( coefficients, answer );
 
     return 0;
@@ -67,7 +76,7 @@ int main( ) {
 //!
 //------------------------------------------------------------------------------------------------------
 
-AnswerQuestion solveEquation( Coefficients coefficients ) {
+SolveResult solveEquation( Coefficients coefficients ) {
 
     double x1 = NAN, x2 = NAN;
     RootsCount countRoots = zeroRoot;
@@ -81,8 +90,7 @@ AnswerQuestion solveEquation( Coefficients coefficients ) {
         solveSquare( coefficients, &x1, &x2, &countRoots );
     }
 
-    AnswerQuestion answer = { x1, x2, countRoots };
-    return answer;
+    return { x1, x2, countRoots };
 }
 
 
@@ -91,6 +99,7 @@ AnswerQuestion solveEquation( Coefficients coefficients ) {
 //! @param [in] coefficients   coefficients - структура, содержащая коэффициенты квадратного уравнения.
 //! @param [in] *x1   *x1 - указатель на меньший корень уравнения.
 //! @param [in] *x2   *x2 - указатель на больший корень уравнения.
+//! @param [in] *countRoots    *countRoots- указатель на количество корней.
 //!
 //! @note Решает квадратное уравнение, записывает результат в x1, x2 и countRoots соответственно.
 //-----------------------------------------------------------------------------------------------------
@@ -110,19 +119,19 @@ void solveSquare( Coefficients coefficients, double* x1, double* x2, RootsCount*
 
         *x1 = firstPart - secondPart;
         *x2 = firstPart + secondPart;
-        *countRoots = ( *x1 == *x2 ) ? twoSameRoot: twoRoot;
+        *countRoots = ( fabs( *x1 - *x2) < delta ) ? twoSameRoot: twoRoot;
     }
 }
 
 //-----------------------------------------------------------------------------------------------------
 //!
 //! @param [in] coefficients   coefficients - структура, содержащая коэффициенты квадратного уравнения.
-//! @param [in] answer         answer - структура, сожеражащая корни уравнения и их количество.
+//! @param [in] answer  answer - структура, сожеражащая корни уравнения и их количество.
 //!
 //! @note Выводит соответсвующие пояснение и решение.
 //!
 //-----------------------------------------------------------------------------------------------------
-void printResult( Coefficients coefficients, AnswerQuestion answer ) {
+void printResult( Coefficients coefficients, SolveResult answer ) {
 
     printf("\na: %lg b: %lg c: %lg\n", coefficients.a, coefficients.b, coefficients.c );
 
@@ -135,7 +144,8 @@ void printResult( Coefficients coefficients, AnswerQuestion answer ) {
 
         case twoRoot:
 
-            printf("\nУравнение квадратное.\nМеньший корень: %lg\nБольший корень: %lg", answer.x1, answer.x2 );
+            printf("\nУравнение квадратное.\nМеньший корень: %lg\nБольший корень: %lg",
+            answer.x1, answer.x2 );
             break;
 
         case oneRoot:
@@ -164,6 +174,7 @@ void printResult( Coefficients coefficients, AnswerQuestion answer ) {
 //! @param [in] coefficients   coefficients - структура, содержащая коэффициенты квадратного уравнения.
 //! @param [in] *x1   *x1 - указатель на меньший корень уравнения.
 //! @param [in] *x2   *x2 - указатель на больший корень уравнения.
+//! @param [in] *countRoots    *countRoots- указатель на количество корней.
 //!
 //! @note Решает линейное уравнение, результат записывает в x1, x2 и countRoots соотвественно.
 //!
@@ -185,3 +196,86 @@ void solveLinear( Coefficients coefficients, double* x1, double* x2, RootsCount*
     }
 
 }
+
+int testSolve( SolveResult truResult, SolveResult* actualResult ) {
+
+    if ( isnan(truResult.x1) && isnan(truResult.x2) && isnan( (*actualResult).x1 )
+    && isnan( (*actualResult).x2 ) ) {
+
+        return 1;
+    }
+
+    else if ( fabs( truResult.x1 - (*actualResult).x1 ) <= delta  &&
+    fabs( truResult.x2 - (*actualResult).x2 <= delta )
+    && fabs( truResult.countRoots - (*actualResult).countRoots) <= delta) {
+
+        return 1;
+    }
+    else {
+
+        return 0;
+    }
+}
+
+void printFail( Coefficients coeff, SolveResult truResult, SolveResult* actualResult ) {
+
+    printf("\n\nFALED: solveEquation( %lg %lg %lg ) -> x1 = %lg, x2 = %lg, countRoots = %d"
+    " should be( x1 = %lg, x2 = %lg, countRoots = %d)", coeff.a, coeff.b, coeff.c,
+    (*actualResult).x1, (*actualResult).x2, (*actualResult).countRoots,
+    truResult.x1, truResult.x2, truResult.countRoots );
+
+}
+
+ void test() {
+
+    Coefficients coeff = { 2, 4, 2 };
+    SolveResult truResult = { -1, -1, twoSameRoot };
+    SolveResult actualResult = solveEquation( coeff );
+
+    if (!testSolve( truResult, &actualResult ) ) {
+
+        printFail( coeff, truResult, &actualResult );
+    }
+
+    coeff = { 1.2, -123.7, 11.1};
+    truResult = { 0.0898115, 102.993522, twoRoot };
+    actualResult = solveEquation( coeff );
+
+    if (!testSolve( truResult, &actualResult ) ) {
+
+        printFail( coeff, truResult, &actualResult );
+    }
+
+    coeff = { 0, 0, 0 };
+    truResult = { NAN, NAN, alotRoot };
+    actualResult = solveEquation( coeff );
+
+    if (!testSolve( truResult, &actualResult ) ) {
+
+        printFail( coeff, truResult, &actualResult );
+    }
+
+    coeff = { 0, 1, 0 };
+    truResult = { 0, 0, oneRoot };
+    actualResult = solveEquation( coeff );
+
+    if (!testSolve( truResult, &actualResult ) ) {
+
+        printFail( coeff, truResult, &actualResult );
+    }
+
+    coeff = { 0, 0, 1 };
+    truResult = { NAN, NAN, zeroRoot };
+    actualResult = solveEquation( coeff );
+
+    if (!testSolve( truResult, &actualResult ) ) {
+
+        printFail( coeff, truResult, &actualResult );
+    }
+
+
+ }
+
+
+
+
