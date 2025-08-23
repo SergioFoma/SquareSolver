f976aebe505b7eeecd8539d950ab271db39827ad
SergioFoma <s.kuzminsky.2007@gmail.com>
2025-08-23 20:03:18 +0300

    Сделал разбиение на файлы

---
+++ square.cpp
@@ -0,0 +1,103 @@
+#include <TXLib.h>
+#include <stdio.h>
+#include "solver.h"
+#include "test.h"
+#include "goTest.h"
+
+#include <math.h>
+
+void printResult( Coefficients coefficients, SolveResult answer );
+
+int inputCoefficient( Coefficients* coefficients );
+
+int main( int argc, char **argv ) {
+
+
+    if ( goTest( argc, argv ) ) {
+
+        return 0;
+    }
+
+    printf("Введите коэффициенты квадратного уравнения( a, b и c соответсвенно)"
+           " на следующей строке через пробел: \n\n");
+
+    Coefficients coefficients = {0, 0, 0};
+
+    if (!inputCoefficient( &coefficients) ) {
+
+        return 0;
+    }
+
+    SolveResult answer = solveEquation( coefficients );
+    printResult( coefficients, answer );
+
+    return 0;
+}
+
+//-----------------------------------------------------------------------------------------------------
+//!
+//! @param [in] coefficients   coefficients - структура, содержащая коэффициенты квадратного уравнения.
+//! @param [in] answer  answer - структура, сожеражащая корни уравнения и их количество.
+//!
+//! @note Выводит соответсвующие пояснение и решение.
+//!
+//-----------------------------------------------------------------------------------------------------
+
+void printResult( Coefficients coefficients, SolveResult answer ) {
+
+    printf("\na: %lg b: %lg c: %lg\n", coefficients.a, coefficients.b, coefficients.c );
+
+    switch ( answer.countRoots ) {
+
+        case zeroRoot:
+
+            printf("\nНет корней.");
+            break;
+
+        case twoRoot:
+
+            printf("\nУравнение квадратное.\nМеньший корень: %lg\nБольший корень: %lg",
+            answer.x1, answer.x2 );
+            break;
+
+        case oneRoot:
+
+            printf("\nУравнение линейное.\nКорень уравнения: %lg", answer.x1 );
+            break;
+
+        case alotRoot:
+
+            printf("\nУравнение имеет бесконечное количество корней.");
+            break;
+
+        case twoSameRoot:
+
+            printf("\nУравнение квадратное.\nИмеет единственный корень: %lg", answer.x1 );
+            break;
+
+        default:
+
+            printf("\nОшибка. Не определено количество корней.");
+            break;
+    }
+}
+
+int inputCoefficient( Coefficients* coefficients) {
+
+
+    if ( scanf("%lg %lg %lg", &( (*coefficients).a ), &( (*coefficients).b ), &( (*coefficients).c ) ) < 3 ) {
+
+        printf("\nОшибка. Введен неверный формат квадратного уравнения.");
+        return 0;
+    }
+
+    if ( !isfinite( (*coefficients).a ) || !isfinite( (*coefficients).b ) ||
+         !isfinite( (*coefficients).c) ) {
+
+            return 0;
+    }
+
+
+    return 1;
+
+}
